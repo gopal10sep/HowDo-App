0e15da0e4a8df03fde9deceb154e25a17610f54d
Gopal Kumar <gopalkmr19@gmail.com>
2024-07-05 22:43:23 -0700

    Fixed build errors- Continued

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -14,32 +14,25 @@ export async function POST(req: Request) {
   try {
     const { messages } = await req.json();
     const lastMessage = messages[messages.length - 1].content;
-
     const pinecone = new Pinecone({ apiKey: pineconeKey });
     const pineconeIndex = pinecone.index(indexName);
-
     const genAI = new GoogleGenerativeAI(googleApiKey);
     const model = genAI.getGenerativeModel({ model: "text-embedding-004" });
     const res = await model.embedContent(lastMessage);
     const embedding = res.embedding;
-
     const similarityResponse = await pineconeIndex.query({
       topK: 3,
       vector: embedding.values,
       includeValues: true,
       includeMetadata: true,
     });
-
     const fetchedContextArray = similarityResponse.matches.map(match => ({
       score: match.score,
       metadata: match.metadata,
     }));
-
     const fetchedContext = fetchedContextArray.map(item => JSON.stringify(item)).join('\n');
-
     const SYSTEM_TEMPLATE = fs.readFileSync(SYSTEM_PROMPT_PATH, 'utf-8');
     const SYSTEM_PROMPT_COMPLETE = `${SYSTEM_TEMPLATE}\n\n${fetchedContext}`;
-
     const result = await streamText({
       model: google('models/gemini-1.5-flash'),
       messages: [
@@ -48,7 +41,6 @@ export async function POST(req: Request) {
         { role: 'user', content: lastMessage },
       ],
     });
-
     return result.toAIStreamResponse();
   } catch (error) {
     console.error('Error in POST route:', error);
--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,27 @@
-// import { authMiddleware } from "@clerk/nextjs";
 
-// // This example protects all routes including api/trpc routes
-// // Please edit this to allow other routes to be public as needed.
-// // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
-// export default authMiddleware({
-//   publicRoutes: ["/", "/api/webhook"],
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+// const isPublicRoute = createRouteMatcher([
+//   '/',
+//   '/api/webhook'
+// ]);
+
+// export default clerkMiddleware((auth, req) => {
+//   if (!isPublicRoute(req)) {
+//     auth().protect();
+//   }
 // });
 
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
 
-const isPublicRoute = createRouteMatcher([
-  '/',
-  '/api/webhook'
-]);
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
-    auth().protect();
-  }
-});
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
